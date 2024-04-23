06b1a376af08a6721e7b7d4a4a90177d4238fbfe
mmillan18 <2220211035@estudiantesunibague.edu.co>
2024-04-22 22:32:50 -0500

    fix

--- crudApp/urls.py
+++ crudApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),  # La URL base ahora apunta a la vista 'home'
+    path('', views.home, name='home'),  # La URL base ahora apunta a la vista 'home'
+    path('usuarios/', views.lista_usuarios, name='lista_usuarios'),
 ]
--- crudApp/views.py
+++ crudApp/views.py
@@ -1,5 +1,21 @@
+from django.http import HttpResponse
+from django.views.decorators.cache import never_cache
+import requests
 from django.shortcuts import render
+from .models import Usuario
 
+
+
+@never_cache
+def lista_usuarios(request):
+    response = requests.get('http://localhost:8080/usuarios')
+    if response.ok:
+        usuarios = response.json()
+        return render(request, 'crudApp/lista_usuarios.html', {'usuarios': usuarios})
+    else:
+        return HttpResponse('Error al obtener los usuarios: ' + str(response.status_code))
+
+@never_cache
 def home(request):
     # El argumento de 'render' debe ser 'crudApp/home.html', no 'home.html' si sigues la convención estándar.
     return render(request, 'crudApp/home.html')
