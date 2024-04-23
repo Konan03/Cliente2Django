e60c719c6447c29db42f8d9a7fa00c27bc82731d
mmillan18 <2220211035@estudiantesunibague.edu.co>
2024-04-22 23:17:52 -0500

    add navbar

--- crudApp/views.py
+++ crudApp/views.py
@@ -5,9 +5,7 @@ from django.views.decorators.cache import never_cache
 import requests
 from django.shortcuts import render
 from .models import Usuario
-
-
-
+from django.views import View
 @never_cache
 def lista_usuarios(request):
     response = requests.get('http://localhost:8080/usuarios')
@@ -17,7 +15,26 @@ def lista_usuarios(request):
     else:
         return HttpResponse('Error al obtener los usuarios: ' + str(response.status_code))
 
+from django.shortcuts import render
+
 @never_cache
 def home(request):
-    # El argumento de 'render' debe ser 'crudApp/home.html', no 'home.html' si sigues la convención estándar.
-    return render(request, 'crudApp/home.html')
+    # Aquí puedes agregar cualquier contexto que desees pasar a tu plantilla
+    context = {
+        # Por ejemplo, si quieres mostrar un mensaje de bienvenida dinámico
+        'welcome_message': '¡Bienvenido a nuestra aplicación!',
+        # Otros datos necesarios para la página de inicio
+    }
+    return render(request, 'crudApp/home.html', context)
+
+class UserView(View):
+    def get(self, request):
+        return render(request, 'usuario.html')
+
+class VideogameView(View):
+    def get(self, request):
+        return render(request, 'videojuego.html')
+
+class AboutView(View):
+    def get(self, request):
+        return render(request, 'about.html')
